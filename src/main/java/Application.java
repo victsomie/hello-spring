a7ef9fb29393a9c27c1dee02ee1b7c2a6ad9566d
victsomie <victsomie@yahoo.com>
2018-05-19 11:32:15 +0300

    Fisrt configureations on XML

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import service.CustomerService;
 import service.CustomerServiceImpl;
 
 public class Application{
 
     public static void main(String[] args){
-        CustomerService service = new CustomerServiceImpl();
+        // CustomerService service = new CustomerServiceImpl();
+
+        // Initialize the application context
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
 
         System.out.println("First Name: " + service.findAll().get(0).getFirstName());
         //System.out.println("FirstName >> "+ ((CustomerServiceImpl) service).findAll().get(0).getFirstName());
